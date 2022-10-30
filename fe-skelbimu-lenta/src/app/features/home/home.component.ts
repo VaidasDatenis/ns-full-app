68bb4cccb379862a91246665e37bc65c9d78c338
Vaidas Datenis <datenis.v@gmail.com>
2022-10-30 21:30:28 +0200

    fixes

--- fe-skelbimu-lenta/src/app/features/home/home.component.ts
+++ fe-skelbimu-lenta/src/app/features/home/home.component.ts
@@ -8,14 +8,23 @@ import { FlickService } from '../../code/services/flick.service'
 @Component({
   moduleId: module.id,
   selector: 'ns-home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss']
 })
 export class HomeComponent {
   flicks = this.flickService.getFlicks()
 
-  constructor(private flickService: FlickService, private routerExtensions: RouterExtensions) {}
+  constructor(private flickService: FlickService, private routerExtensions: RouterExtensions) { }
 
   onFlickTap(args: ItemEventData): void {
     this.routerExtensions.navigate(['details', this.flicks[args.index].id])
+  }
+
+  handleSignIn() {
+    console.log('sign in!')
+  }
+
+  handleSignUp() {
+    console.log('sign up!')
   }
 }
