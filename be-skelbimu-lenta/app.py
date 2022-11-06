6368627f7b00501997ee7a3438fa5ec16619a823
VaidasDatenis <datenis.v@gmail.com>
2022-11-06 17:41:24 +0200

    added Tag Models

--- be-skelbimu-lenta/app.py
+++ be-skelbimu-lenta/app.py
@@ -1,13 +1,12 @@
-import os
-
 from flask import Flask
 from flask_smorest import Api
 
-from db import db
 import models
 
+from db import db
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 
 def create_app(db_url=None):
@@ -20,8 +19,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     # app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     # app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn."
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
-        "DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
@@ -32,5 +30,6 @@ def create_app(db_url=None):
     api = Api(app)
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
