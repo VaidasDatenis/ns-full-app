850fbf6341632a838502ad5ef58a941050dd3f21
VaidasDatenis <datenis.v@gmail.com>
2022-11-07 15:37:44 +0200

    JWT token refresher

--- be-skelbimu-lenta/app.py
+++ be-skelbimu-lenta/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
-from flask_smorest import Api
+import os
+import secrets
 
-import models
+from flask import Flask, jsonify
+from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
+import models
+
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -19,9 +25,72 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     # app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     # app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn."
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
+    app.config["JWT_SECRET_KEY"] = "jose"
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is fresh.",
+                    "error": "fresh_token_required"
+                }
+            ),
+            401,
+        )
+
+    # @jwt.additional_claims_loader
+    # def add_claims_to_jwt(identity):
+    #     if identity == 1:
+    #         return {"id_admin": True}
+    #     return {"id_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"message": "The token has expired.", "error": "token_expired"}
+            ),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signiture verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "describtion": "Request does not contain an access token.",
+                    "error": "authorization_required"}
+            ),
+            401,
+        )
 
     @app.before_first_request
     def create_tables():
@@ -31,5 +100,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
